5e4605672135323fbb71e0a307d1ae05e4a1eab4
Keigo Kawamura <kkawamura@preferred.jp>
2021-04-17 10:50:09 +0900

    Add key

--- src/components/inputPage/inputPage.tsx
+++ src/components/inputPage/inputPage.tsx
@@ -7,7 +7,7 @@ import styles from "./styles.module.scss";
 export const InputPage = () => {
   const dispatch = useDispatch();
   const talismans = useSelector(selectTalismanAll).map((talisman) => (
-    <p>{talisman.name}</p>
+    <p key={talisman.id}>{talisman.name}</p>
   ));
   return (
     <div>
